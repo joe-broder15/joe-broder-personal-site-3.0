b3679375b8013d4c143db3088f6ce36546075121
joe-broder15 <joe.broder@berkeley.edu>
2020-11-04 20:35:18 -0800

    push to desktop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,57 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1> Hello, World! </h1>
+                <div className="homepage-section">
+                    <h1 id="title"> Joe Broder</h1>
+                    <div className="homepage-links">
+                        <a href="#">
+                            <button>email</button>
+                        </a>
+                        <a href="#">
+                            <button>github</button>
+                        </a>
+                        <a href="#">
+                            <button>linkedin</button>
+                        </a>
+                        <a href="#">
+                            <button>resume</button>
+                        </a>
+                        <a href="#">
+                            <button>blog</button>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="homepage-section">
+                    <h2>About Me</h2>
+                    <p>
+                        My name is Joe, I'm a rising senior EECS (Electrical
+                        Engineering and Computer Science) major at UC Berkeley
+                        interested in security, cryptography, and software
+                        engineering. I'm a full-stack blockchain, web, and
+                        mobile developer with experience using Solidity,
+                        Web3.js, MERN stack, Flask, Bootstrap, and SQL. Over the
+                        past year I've been a software development consultant at
+                        blockchain at berkeley where I build dApps and work on
+                        large scale solutions for clients. My favorite quote is
+                        "If I had more time I'd have written less code"
+                    </p>
+                </div>
+                <div className="homepage-section">
+                    <h2>Personal Projects</h2>
+                    <p>
+                        My name is Joe, I'm a rising senior EECS (Electrical
+                        Engineering and Computer Science) major at UC Berkeley
+                        interested in security, cryptography, and software
+                        engineering. I'm a full-stack blockchain, web, and
+                        mobile developer with experience using Solidity,
+                        Web3.js, MERN stack, Flask, Bootstrap, and SQL. Over the
+                        past year I've been a software development consultant at
+                        blockchain at berkeley where I build dApps and work on
+                        large scale solutions for clients. My favorite quote is
+                        "If I had more time I'd have written less code"
+                    </p>
+                </div>
             </div>
         );
     }
